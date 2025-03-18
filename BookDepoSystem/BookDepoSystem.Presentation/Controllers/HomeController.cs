4857d6f40a3859dfb7306eaae357ccc69dc6888c
aibaev20 <AIBaev20@codingburgas.bg>
2025-03-18 20:57:18 +0200

    Generate reports

--- BookDepoSystem/BookDepoSystem.Presentation/Controllers/HomeController.cs
+++ BookDepoSystem/BookDepoSystem.Presentation/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookDepoSystem.Presentation.Controllers;
 
@@ -18,17 +19,20 @@ public class HomeController : Controller
     private readonly ICurrentUser currentUser;
     private readonly UserManager<ApplicationUser> userManager;
     private readonly ILogger<HomeController> logger;
+    private readonly EntityContext context;
 
     public HomeController(
         IEmailService emailService,
         ICurrentUser currentUser,
         UserManager<ApplicationUser> userManager,
-        ILogger<HomeController> logger)
+        ILogger<HomeController> logger,
+        EntityContext context)
     {
         this.emailService = emailService;
         this.currentUser = currentUser;
         this.userManager = userManager;
         this.logger = logger;
+        this.context = context;
     }
 
     [HttpGet("/")]
@@ -37,6 +41,43 @@ public class HomeController : Controller
     // to see Index first must be logged in
     public async Task<IActionResult> Index()
     {
+        var availableBooksCount = this.context.Books.Where(b => b.QuantityAvailable > 0).Count();
+        var allBooksCount = this.context.Books.Count();
+
+        var popularBooks = this.context.Rents
+            .GroupBy(r => r.BookId)
+            .Select(g => new
+            {
+                BookTitle = g.First().Book!.Title,
+                RentCount = g.Count(),
+            })
+            .OrderByDescending(b => b.RentCount)
+            .Take(3)
+            .ToList();
+
+        var mostActiveRenters = this.context.Rents
+            .GroupBy(r => r.RenterId)
+            .Select(g => new
+            {
+                RenterName = g.First().Renter!.Name,
+                RentCount = g.Count(),
+            })
+            .OrderByDescending(b => b.RentCount)
+            .Take(3)
+            .ToList();
+
+        var completedRents = this.context.Rents
+            .Where(r => r.Status == "Overdue")
+            .Count();
+        var allRentsCount = this.context.Rents.Count();
+
+        this.ViewBag.AvailableBooksCount = availableBooksCount;
+        this.ViewBag.AllBooksCount = allBooksCount;
+        this.ViewBag.PopularBooks = popularBooks;
+        this.ViewBag.MostActiveRenters = mostActiveRenters;
+        this.ViewBag.CompletedRents = completedRents;
+        this.ViewBag.AllRentsCount = allRentsCount;
+
         return this.View();
     }
 
