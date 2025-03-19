eb2810a458271d54fe868919875fe39f473d5a64
aibaev20 <AIBaev20@codingburgas.bg>
2025-03-19 15:49:59 +0200

    Implement project testing

---
+++ BookDepoSystem/BookDepoSystem.Services.Tests/BookTests.cs
@@ -0,0 +1,57 @@
+﻿using BookDepoSystem.Data.Models;
+using BookDepoSystem.Services.Implementations;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace BookDepoSystem.Services.Tests;
+
+public class BookTests
+{
+    [Fact]
+    public async Task GetByIdAsync_OnExistingBook_ShouldReturnBookModel()
+    {
+        var dbContext = TestHelpers.CreateDbContext();
+
+        var newBook = new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "Test Title",
+            Author = "Test Author",
+            Genre = "Test Genre",
+            Location = "Test Location",
+            Information = "Test Information",
+            PublishedDate = new DateTime(2025, 3, 19),
+            QuantityAvailable = 10,
+        };
+
+        dbContext.Books.Add(newBook);
+        await dbContext.SaveChangesAsync();
+
+        var service = new BookService(dbContext);
+        
+        var retrievedBook = await service.GetBookById(newBook.BookId);
+        
+        retrievedBook.Should().NotBeNull();
+        retrievedBook!.BookId.Should().Be(newBook.BookId);
+        retrievedBook.Title.Should().Be("Test Title");
+        retrievedBook.Information.Should().Be("Test Information");
+
+        await dbContext.DisposeAsync();
+        
+        /*var booksPaginated = await service.GetBooksPaginated("", 1, 5);
+        booksPaginated.Should().NotBeNull();
+        booksPaginated.Books.Should().NotBeEmpty();*/
+        
+        /*var firstBook = booksPaginated.Books.FirstOrDefault();
+        firstBook.Should().NotBeNull();
+        retrievedBook!.BookId.Should().Be(bookId);
+        /*firstBook.BookId.Should().Be(BookId); // Use the bookId variable instead of Guid.NewGuid()#1#
+        firstBook.Title.Should().Be("Test Title");
+        firstBook.Information.Should().Be("Test Information");*/
+        
+        /*booksPaginated.Id.Should().Be(Guid.NewGuid());
+        booksPaginated.Title.Should().Be("Test Title");
+        booksPaginated.Information.Should().Be("Test Information");*/
+    }
+}
---
+++ BookDepoSystem/BookDepoSystem.Services.Tests/TestHelpers.cs
@@ -0,0 +1,16 @@
+using System;
+using BookDepoSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookDepoSystem.Services.Tests;
+
+public static class TestHelpers
+{
+    public static EntityContext CreateDbContext()
+    {
+        var dbContextOptions = new DbContextOptionsBuilder<EntityContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString());
+        
+        return new EntityContext(dbContextOptions.Options);
+    }
+}
